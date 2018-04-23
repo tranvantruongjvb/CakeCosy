5bd97e0e5c8095ce132bb881169d31b075e6b8b9
tranvantruongjvb <34053656+tranvantruongjvb@users.noreply.github.com>
2018-04-23 17:56:27 +0700

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -80,7 +80,7 @@ class AppController extends Controller
 	public function beforeFilter(Event $event)
 	{
 		
-		$this->Auth->allow(['index','contact','typeProduct','detailProduct','getSearch','getAddToCart','destroy','order','deleteItem','updateQuantity','addUser','postCheckout','getNew','forgetPass', 'viewMoreProduct']);
+		$this->Auth->allow(['index','contact','typeProduct','detailProduct','getSearch','getAddToCart','destroy','order','deleteItem','updateQuantity','addUser','postCheckout','getNew','forgetPass', 'viewMoreProduct', 'news', 'introducttion']);
 	}
     /**
      * Before render callback.
